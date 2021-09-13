e18076a5edc8e018bab734edd631a1ba9369878c
Samuel McKnight <samuellp@gmail.comgit statusgit config --global user.user.email iamsamuellp@gmail.com>
2021-09-13 01:08:37 -0500

    Added App.js to components folder.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,31 @@
 import React, {Component} from 'react';
 import'./App.css';
-
+import axios from 'axios';
 
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = { 
+      songs: []
+
+     }
   }
+  componentDidMount(){
+    this.getAllSongs();
+
+  }
+  async getAllSongs(){
+    let resonse = await axios.get('http://127.0.0.1:8000/music/');
+    this.setState({
+      songs: resonse.data
+    })
+  }
+
   render() { 
     return (
-      <div className="container-fluid">
-
+      <div className="App">
+        <h1>Hello World!</h1>
       </div>
       );
   }
