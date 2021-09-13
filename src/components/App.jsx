3f9c72c084f728e53085369f56381c5c500ad671
Samuel McKnight <samuellp@gmail.comgit statusgit config --global user.user.email iamsamuellp@gmail.com>
2021-09-13 12:06:51 -0500

    In MusicLibraryForm I added an add button to that allows me to post a new song to the backend api.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -28,18 +28,30 @@ class App extends Component {
       console.log('Error in Api call!');
     }
   }
-  createSong=(song)=>{
+  aSong=(song)=>{
     this.songs.push(song);
     this.setState({
       oneSong:this.songs.length -1
     })
+  }
+  async newSong(sing){
+    try{
+      let response = await axios.post('http://127.0.0.1:8000/music/',sing)
+      this.setState({
+        everySong:response.data
+      });
+      this.makeGetRequest()
+    }
+    catch (error) {
+      console.log('Error in Create API call!');
+  }
   }
 
   render() { 
     return (
       <React.Fragment>
         <h1>Hello World!</h1>
-        <MusicLibraryForm everySong={this.createSong}/>
+        <MusicLibraryForm newSong={this.newSong.bind(this)}/>
         <MusicLibraryTable sing={this.state.songs}/>
         
       </React.Fragment>
--- src/components/MusicLibraryForm/MusicLibraryForm.jsx
+++ src/components/MusicLibraryForm/MusicLibraryForm.jsx
@@ -3,35 +3,49 @@ import React, { Component } from 'react';
 
 
 
-class MusicLibrary extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
-        title:"",
-        album:"",
-        artist:"",
-        genre:"",
-        releaseDate:""
+class MusicLibraryForm extends Component {
+    state = { 
+        title:'',
+        album:'',
+        artist:'',
+        genre:'',
+        release_date:''
      }
-  }
+ 
 
   handleChange=(event) =>{
     this.setState ({
-      [event.target.title]: event.target.value,
+      [event.target.name]: event.target.value,
     });
-  }
+  };
  
   handleSubmit=(event) =>{
     event.preventDefault();
-    this.props.everySongs(this.state);
-  }
+    let sing ={
+      title:this.state.title,
+      artist:this.state.album,
+      album:this.state.album,
+      genre:this.state.genre,
+      release_date:this.state.release_date,
+    }
+
+    this.props.newSong(sing);
+    this.setState({
+      title:'',
+      album:'',
+      artist:'',
+      genre:'', 
+      release_date:'',
+
+    });
+  };
 
 
 
 
   render() { 
     return (
-      <form onSubmit={this.handleSubmit}> 
+      <form onSubmit={(event) => this.handleSubmit(event)}> 
         <label>Song Title</label>
              <input name="title" onChange={this.handleChange} value={this.state.title}/>
              <label>Album</label>
@@ -42,10 +56,10 @@ class MusicLibrary extends Component {
              <input name="genre" onChange={this.handleChange} value={this.state.genre}/>
              <label>release Date</label>
              <input name="release_date" onChange={this.handleChange} value={this.state.release_date}/>
-             <button type="submit">Enter Song</button>
+             <button type="submit" onClick={this.sing}>New Song</button>
       </form>
       );
   }
 }
  
-export default MusicLibrary ;
+export default MusicLibraryForm 
